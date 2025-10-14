20620ae06e37beb9e76482b582e6066db632eaf4
Willian Padilha <45665895+Williais@users.noreply.github.com>
2025-10-14 17:49:07 -0300

    escondendo chaves

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://minswminvxbyyebosefo.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pbnN3bWludnhieXllYm9zZWZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAyMjgwMjcsImV4cCI6MjA3NTgwNDAyN30.HqcecU5SnGxArS0aomVbR7swQ_QJ7VMSY_0dGlRVIKw';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
